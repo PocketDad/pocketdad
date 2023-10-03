6e4ef12ced18c9bebb65f14ecdccad3ca9b19c70
kimsyd <kimsyd@hawaii.edu>
2023-10-03 11:50:42 -1000

    buttons with images
    to do: add pressed color

--- lib/main.dart
+++ lib/main.dart
@@ -192,47 +192,23 @@ class ChooseAvatar extends StatelessWidget {
 }
 
 class Profile extends StatelessWidget{
+  @override
   Widget build(BuildContext context) {
     return Container(
       height: 80,
       width: 80,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.deepOrange, width: 2),
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
       child: FloatingActionButton(
       backgroundColor: Colors.orange,
       onPressed: () {},
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-      tooltip: 'Increment',
-      child: const Icon(Icons.insert_emoticon),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+          child: Image.asset("assets/images/cat2.png")
       ),
-    );/*ElevatedButton(
-        style: ButtonStyle(
-          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-          overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                (Set<MaterialState> states) {
-              if (states.contains(MaterialState.hovered)) {
-                return Colors.orangeAccent.withOpacity(0.04);
-              }
-              if (states.contains(MaterialState.focused) || states.contains(MaterialState.pressed)) {
-                return Colors.orangeAccent.withOpacity(0.12);
-              }
-              return null; // Defer to the widget's default.
-            },
-          ),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero,
-                    side: BorderSide(color: Colors.red)
-                )
-            )
-        ),
-        onPressed: () {},
-        child: SizedBox(width: 0, height: 10),
-
-      /*Container(
-          color: Colors.orange,
-          height: 50,
-          width: 50,
-        ),*/
-      );*/
+      ),
+    );
   }
 }
