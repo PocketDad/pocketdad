cc0a065811994e1161839d98c75a58e16e7358a0
yongsungm <97987234+yongsungm@users.noreply.github.com>
2023-10-08 21:41:38 -1000

    Update home_view.dart

--- lib/src/views/home/home_view.dart
+++ lib/src/views/home/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pocketdad/src/views/home/pages/add_task.dart';
 import 'package:pocketdad/src/views/home/pages/chat.dart';
 import 'package:pocketdad/src/views/home/pages/check_in.dart';
+import 'package:pocketdad/src/views/home/pages/list_tasks.dart';
 
 /// Top-level Layout for all of the "Home" related
 class HomeView extends StatefulWidget {
@@ -15,8 +16,8 @@ class HomeView extends StatefulWidget {
   // need to be moved into the state widget.
   final Map pages = {
     0: {
-      'title': const Text('Check In'),
-      'body': const CheckIn(),
+      'title': const Text('Tasks'),
+      'body': ListTasks(),
       'navItem': const BottomNavigationBarItem(
         icon: Icon(Icons.checklist),
         label: "Tasks",
@@ -31,6 +32,14 @@ class HomeView extends StatefulWidget {
       ),
     },
     2: {
+      'title': const Text('Check In'),
+      'body': const CheckIn(),
+      'navItem': const BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: "Check In",
+      ),
+    },
+    3: {
       'title': const Text('Camera'),
       'body': const Chat(),
       'navItem': const BottomNavigationBarItem(
@@ -45,7 +54,7 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 2;
 
   void _onItemTapped(int index) {
     if (index != _selectedIndex) {
@@ -66,6 +75,7 @@ class _HomeViewState extends State<HomeView> {
           widget.pages[0]['navItem'],
           widget.pages[1]['navItem'],
           widget.pages[2]['navItem'],
+          widget.pages[3]['navItem'],
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
