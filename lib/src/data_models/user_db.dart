918ced72e53e8aa2722544d196637ca7db9eed24
justinjandoc <102justinjandoc@gmail.com>
2023-10-10 10:39:10 -1000

    Issue #19: Added user fields

--- lib/src/data_models/user_db.dart
+++ lib/src/data_models/user_db.dart
@@ -1,1 +1,18 @@
-
+class UserData {
+  UserData(
+    {
+      required this.id,
+      required this.name,
+      required this.email,
+      required this.username,
+      this.dadName = "PocketDad",
+      this.dadPic = "assets/images/dad1.JPG"
+    }
+  );
+  String id;
+  String name;
+  String email;
+  String username;
+  String dadName;
+  String dadPic; // default should be some random picture
+}
