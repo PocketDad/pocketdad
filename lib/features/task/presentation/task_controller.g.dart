cd82854c67cf017a3863d37621e2b85e548c0caf
justinjandoc <102justinjandoc@gmail.com>
2023-11-20 23:47:39 -1000

    Create task_controller.g.dart

---
+++ lib/features/task/presentation/task_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$taskControllerHash() => r'2879a5b20af4b0bb47d030e0d02a735680dd8688';
+
+/// See also [TaskController].
+@ProviderFor(TaskController)
+final taskControllerProvider =
+    AutoDisposeAsyncNotifierProvider<TaskController, void>.internal(
+  TaskController.new,
+  name: r'taskControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$taskControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TaskController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter, deprecated_member_use_from_same_package
