748cd62e53b84863137ea978607a9aed1b6ebf9f
kimsyd <kimsyd@hawaii.edu>
2023-12-05 17:46:14 -1000

    list tasks page using singletaskcard

--- lib/features/task/presentation/single_task_card.dart
+++ lib/features/task/presentation/single_task_card.dart
@@ -11,51 +11,49 @@ class SingleTaskCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Card(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-                Radius.circular(15)
-            )
-        ),
-        color: Theme.of(context).colorScheme.surface,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+              Radius.circular(15)
+          )
+      ),
+      color: Theme.of(context).colorScheme.surface,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  padding: const EdgeInsets.all(15),
+                  textStyle: Theme.of(context).textTheme.titleLarge,
+                  minimumSize: const Size.fromHeight(50),
                 ),
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    padding: const EdgeInsets.all(15),
-                    textStyle: Theme.of(context).textTheme.titleLarge,
-                    minimumSize: const Size.fromHeight(50),
-                  ),
-                  onPressed: () {  },
-                  child: Text(
-                    task,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
+                onPressed: () {  },
+                child: Text(
+                  task,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onPrimary,
                   ),
                 ),
               ),
-              SizedBox(height: 10,),
-              Text(item),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 10, 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(date),
-                  ],
-                ),
-              )
-            ]
-        ),
+            ),
+            SizedBox(height: 10,),
+            Text(item),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 10, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(date),
+                ],
+              ),
+            )
+          ]
       ),
     );
   }
